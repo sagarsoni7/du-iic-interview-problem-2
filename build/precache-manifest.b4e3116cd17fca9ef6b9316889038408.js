bdc50d1146a83b64368abab6228179e38d5f0e67
Sagar Soni <sagar.soni@zoho.com>
2019-12-22 09:31:04 +0530

    html version

---
+++ build/precache-manifest.b4e3116cd17fca9ef6b9316889038408.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77f665e2532d1365b04eff6d04140fb2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "10ea6d64246289325b44",
+    "url": "./static/js/2.d7cfa156.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "./static/js/2.d7cfa156.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dd9f509b74e2970a441b",
+    "url": "./static/js/main.2f38fdf5.chunk.js"
+  },
+  {
+    "revision": "055e6e8b998def3dbbdb",
+    "url": "./static/js/runtime-main.23f73998.js"
+  }
+]);
